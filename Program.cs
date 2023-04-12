0420c0f34c9cc936f4ed6bb53b8b101a10d2a060
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-04-12 20:39:11 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double diagonal;
+            double area;
+            Console.WriteLine("digite o valor da diagonal.");
+          diagonal=double.Parse(Console.ReadLine());
+
+            area = Math.Pow(diagonal, 2) / 2;
+            Console.WriteLine("valor da area é: " + area);
+        }
+    }
+}
